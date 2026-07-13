ba5af089d96c37105edd22c976849e057a8b1ca7
Macnum <w.damilola@yahoo.com>
2026-07-13 23:08:47 +0100

    feat: add ship class

---
+++ src/classes/Ship.js
@@ -0,0 +1,23 @@
+class Ship {
+	#numberOfHits = 0;
+	#length;
+	constructor(length) {
+		if (!Number.isInteger(length) || length <= 0)
+			throw new Error('Length number should be greater than 0');
+		this.#length = length;
+	}
+
+	hit() {
+		if (this.isSunk()) return;
+		this.#numberOfHits++;
+	}
+	isSunk() {
+		return this.#length === this.#numberOfHits;
+	}
+	get length() {
+		return this.#length;
+	}
+	get numberOfHits() {
+		return this.#numberOfHits;
+	}
+}
