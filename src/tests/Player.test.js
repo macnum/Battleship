f1b8b3e79c171f389d3b623f0d125b9d1cb76f7f
Macnum <w.damilola@yahoo.com>
2026-08-06 00:19:09 +0100

    test: add test for Player

---
+++ src/tests/Player.test.js
@@ -0,0 +1,57 @@
+import Player from '../classes/Player.js';
+import Ship from '../classes/Ship.js';
+describe('Player Class', () => {
+	test('Player has a GameBoard', () => {
+		const player1 = new Player();
+
+		expect(player1).toHaveProperty('board');
+		expect(player1.board).toBeDefined();
+	});
+	test('Throws Error when opponent is not a Player', () => {
+		const player1 = new Player();
+
+		expect(() => player1.attack({}, [0, 0])).toThrow(
+			'Opponent must be a Player',
+		);
+		expect(() => player1.attack([], [0, 0])).toThrow(
+			'Opponent must be a Player',
+		);
+		expect(() => player1.attack(null, [0, 0])).toThrow(
+			'Opponent must be a Player',
+		);
+		expect(() => player1.attack(undefined, [0, 0])).toThrow(
+			'Opponent must be a Player',
+		);
+	});
+	test('A successful attack returns true', () => {
+		const player1 = new Player();
+
+		const player2 = new Player();
+		player2.board.placeShip(new Ship(3), [0, 0], 'horizontal');
+
+		expect(player1.attack(player2, [0, 0])).toBe(true);
+		expect(player1.attack(player2, [0, 1])).toBe(true);
+		expect(player1.attack(player2, [0, 2])).toBe(true);
+	});
+	test('Missed attack return false', () => {
+		const player1 = new Player();
+
+		const player2 = new Player();
+		player2.board.placeShip(new Ship(3), [5, 0], 'horizontal');
+
+		expect(player1.attack(player2, [0, 0])).toBe(false);
+		expect(player1.attack(player2, [0, 1])).toBe(false);
+		expect(player1.attack(player2, [0, 2])).toBe(false);
+	});
+	test('Attacking the same cell throws an error', () => {
+		const player1 = new Player();
+
+		const player2 = new Player();
+		player2.board.placeShip(new Ship(3), [0, 0], 'horizontal');
+		player1.attack(player2, [0, 0]);
+
+		expect(() => player1.attack(player2, [0, 0])).toThrow(
+			'Coordinates has been attacked',
+		);
+	});
+});
