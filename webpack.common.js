6aaf33a4753f52731fd7ab55c090cedc724a971d
Macnum <w.damilola@yahoo.com>
2026-08-06 00:03:27 +0100

    chore: update config

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ import path from 'node:path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-	entry: './src/battleship.js',
+	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(import.meta.dirname, 'dist'),
