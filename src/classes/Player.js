226567dc247bcdc4690188f4350d00461dbe573b
Macnum <w.damilola@yahoo.com>
2026-08-11 13:00:58 +0100

    feat: update player board name

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -2,15 +2,18 @@ import GameBoard from './Gameboard.js';
 
 export default class Player {
 	constructor() {
-		this.board = new GameBoard();
+		this.gameBoard = new GameBoard();
 	}
 	attack(opponent, coords) {
 		if (!(opponent instanceof Player)) {
 			throw new Error('Opponent must be a Player');
 		}
-		return opponent.board.receiveAttack(coords);
+		return opponent.gameBoard.receiveAttack(coords);
 	}
 }
 
 const player1 = new Player();
+const player2 = new Player();
+console.log(player2.attack(player1, [0, 0]));
+console.log(player2.gameBoard);
 console.log(player1 instanceof Player);
